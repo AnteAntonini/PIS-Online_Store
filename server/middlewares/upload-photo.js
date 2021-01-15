5e258425afd1974cea4bdd1d8b1f1d18aadae1a4
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-01-15 18:29:51 +0100

    setting up Amazon S3 Bucket

--- server/middlewares/upload-photo.js
+++ server/middlewares/upload-photo.js
@@ -1,11 +1,12 @@
 //Multer is excellent library for our API middleware to get the photo from the frontend
 //Multer-s3 will directly upload whenever you upload a photo from frontend to backend, it will automatically upload it to s3 bucket 
 
+
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-aws.config.update({
+aws.config.update({         
     secretAccessKey: process.env.aws_secret_access_key,
     accessKeyId: process.env.aws_access_key_id,
     aws_session_token: process.env.aws_session_token
