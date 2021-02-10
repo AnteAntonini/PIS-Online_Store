aa8f1b12084b4da559236a4d1247c3de10d127ba
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-02-10 13:43:53 +0100

    export userRoutes

--- server/server.js
+++ server/server.js
@@ -34,11 +34,13 @@ app.use(cors());
 const productRoutes = require('./routes/product');
 const categoryRoutes = require('./routes/category');
 const ownerRoutes = require('./routes/owner');
+const userRoutes = require('./routes/auth');
 
 
 app.use('/api', productRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', ownerRoutes);
+app.use('/api', userRoutes);
 
 
 app.listen(3000, err => {
