4b1e37405df876e80e8aeeee8b46c9314530fffb
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-02-28 16:03:26 +0100

    category filter

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container mb-10">
+    <div style="display: flex" class="align-center">
+    
+    <v-select style="width: 0" :items="items" label="Category" v-model="selectedCategory">
+    </v-select>
+    <v-btn text style="min-width: 20px" class="pa-0 red--text" @click="clearFilter">X</v-btn>
     <v-text-field
         filled
         label="Search products"
@@ -8,6 +13,7 @@
         color="white"
         v-model="search"
       ></v-text-field>
+      </div>
     <featured-product />
     <v-card nuxt :to="`products/${product._id}`" class="mt-10" height="170" v-for="product in filteredProducts" :key="product._id">
       <div class="d-flex flex-no-wrap justify-start">
@@ -34,14 +40,15 @@ import FeaturedProduct from '../components/FeaturedProduct'
 export default {
   data() {
     return {
-      search: ''
+      search: '',
+      items: ['Book','Games', 'Toys'],
+      selectedCategory: []
     }
   },
   components: { FeaturedProduct },
   async asyncData({$axios}) {
     try {
       let response = await $axios.$get('/api/products')
-      console.log(response)
       return {
         products: response.products
       }
@@ -49,14 +56,25 @@ export default {
       console.log(err)
     }
   },
+  methods: {
+    clearFilter() {
+      this.selectedCategory = ''
+    }
+  },
   computed:{
     filteredProducts() {
-      return this.products.filter((product) => {
-        if (this.search == '') {
+      return this.products.filter(product => {
+        /* if (this.search == '') {
+          return product.title
+        } */
+        if(this.selectedCategory == '') {
           return product.title
         }
-        else {
-        return product.title.toLowerCase().match(this.search)
+        else if(this.selectedCategory == product.category.type) {
+          return product.category.type.match(this.selectedCategory)
+        }
+        else{
+          return product.title.toLowerCase().match(this.search)
         }
       })
     }
